fd53466aef6cdd1a226270321c08dead482d263e
Niko Niyazi T*** <71526347+RealTurkishCatRTC@users.noreply.github.com>
2021-01-03 19:49:09 +0300

    Rename Website Önerici 1.0.1.py to Website Önerici 1.0.1 TR.py

--- Website Önerici 1.0.1 TR.py
+++ Website Önerici 1.0.1 TR.py
@@ -1,5 +1,5 @@
-print("-Website Önerici 1.0.1'e Hoşgeldiniz! \n-Başka bir Kategoriye Bakmak için programı yeniden başlatın \n-Daha Çok Websiteler ve Kategoriler Eklenicektir\n")
-soru=input("En Çok Hangisini Yapmaktan Hoşlanırsınız? \n KATEGORİLER \n -Müzik Dinlemek | Seçmek için M yazın \n -Kitap Okumak | Seçmek için K yazın \n -Oyun Oynamak | Seçmek için O yazın \n -Yemek Tariflerine Bakmak | Seçmek için Y yazın \n -Spor Yapmak | Seçmek için S yazın \n -Alışveriş Yapmak | Seçmek için A yazın \n -Film İzlemek | Seçmek için F yazın \n\n")
+print("-Website Önerici 1.0.1'e Bossşgeldiniz! \n-Başka bir Kategoriye Bakmak için programı yeniden başlatın \n-Daha Çok Websiteler ve Kategoriler Eklenicektir\n")
+soru=input("En Çok Hangisini Yapmaktan Bossşlanırsınız? \n KATEGORİLER \n -Müzik Dinlemek | Seçmek için M yazın \n -Kitap Okumak | Seçmek için K yazın \n -Oyun Oynamak | Seçmek için O yazın \n -Yemek Tariflerine Bakmak | Seçmek için Y yazın \n -Spor Yapmak | Seçmek için S yazın \n -Alışveriş Yapmak | Seçmek için A yazın \n -Film İzlemek | Seçmek için F yazın \n\n")
 
 if soru=="M" or soru=="m":
     print("Müzik Kategorisinde Websiteler: \n music.youtube.com Not: Youtube Kullanıcıları İçin Önerilir. \n itunes.com Not: Apple Kullanıcıları İçin Önerilir. \n plaza.one Not: Vaporwave ve Lo-Fi dinlemek için bir website \n spotify.com Not: Çoğu Müzik Burada. \n iheart.com Not: Amerika dışında online dinlenemiyor olsa da, mobil uygulamaları üzerinden dinleyebildiğimiz müzik 'radyoları' topluluğudur. \n")
