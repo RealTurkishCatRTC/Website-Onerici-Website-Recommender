629f5a1b0e78c8a1d680d772ef174ec5e2fb849b
Niko Niyazi T*** <71526347+RealTurkishCatRTC@users.noreply.github.com>
2021-01-04 10:59:09 +0300

    Add files via upload

---
+++ Website Recommender 1.0.0.py
@@ -0,0 +1,17 @@
+print("Copyright (c) 2021 Niko Niyazi T***\n           All rights reserved \n\n-Welcome to Website Recommender 1.0.0!- \nMore Websites Gonna Be Added Soon. \n Made by TurkishCat777/RealTurkishCatRTC(Niko Niyazi T***)")
+soru=input("Select A Category. \n Categories \n -Listening to Music | Write M to Select \n -Reading Books | Write B to Select \n -Playing Games | Write G to Select \n -Looking for Food Recipes | Write F to Select \n")
+
+if soru=="M" or soru=="m":
+    print("Music Category: \n music.youtube.com Note: Recommended for Youtube Users. \n itunes.com Note: Recommended for Apple Users. \n plaza.one Note: Vaporwave and Lo-Fi radio \n spotify.com Note: Most Popular Music Platform. Recommended for all users. \n iheart.com Not: It can not used online(outside of USA only) but it has mobile \n app. \n")
+elif soru=="B" or soru=="b":
+    print("Book Category: \n gutenberg.org Note: In this website, you can find lots of books that don't have copyright. \n manybooks.net Note: None \n freecomputerbooks.com Note: 'Links to Free Computer, Mathematics, Technical \n Books all over the World'")
+elif soru=="G" or soru=="g":
+    print("Games Category: \n steampowered.com Note: Most Games Are Here. So other websites are more rarely \n needed. \n ")
+elif soru=="F" or soru=="f":
+    print("Food Recipes Category: \n allrecipes.com Note: All kinds of recipes")
+else:
+    print("You Wrote The Wrong Thing. Restart the program and try another thing.")
+
+
+          
+    
